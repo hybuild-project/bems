e7309357eb167ae2994170631220c863c0d5cc58
HYBUILD Project <78462235+hybuild-project@users.noreply.github.com>
2021-10-07 00:37:13 +0200

    Create addon.sh

---
+++ docker/pwd/addon.sh
@@ -0,0 +1,13 @@
+# docker exec -it pwd_node-red.1.*** /bin/bash
+cd /data
+npm install node-red-dashboard
+npm install node-red-node-email
+npm install node-red-node-ui-table
+npm install node-red-contrib-influxdb
+npm install node-red-contrib-localtunnel
+# npm install node-red-contrib-modbus
+npm install node-red-contrib-mongodb
+npm install node-red-contrib-opcua
+# npm install node-red-contrib-string
+npm install node-red-contrib-telegrambot
+npm install node-red-contrib-telegrambot-home
